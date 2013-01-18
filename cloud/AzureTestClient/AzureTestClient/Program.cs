2798e2afc1c324c21fcbbefa08289059782ff69f
Vladimir Marinkovic <marinkovicvlado@gmail.com>
2013-01-18 23:00:56 +0100

    Cloud: initial commit

---
+++ cloud/AzureTestClient/AzureTestClient/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzureTestClient.ServiceReference1;
+
+namespace AzureTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Service1Client client = null;
+
+            try
+            {
+                //GetData test
+                client = new Service1Client();
+                Crop crop = new Crop();
+                crop.Name = "Kukuruz";
+                crop.CropType = "MN98";
+                crop.FertilizingTime = new DateTime(2013, 04, 16);
+                crop.FieldFK = 2;
+                crop.HarvestTime = new DateTime(2013, 05, 05);
+                crop.HillingTime = new DateTime(2013, 03, 03);
+                crop.IllnessFK = 1;
+                crop.JournalFK = 1;
+                crop.timeOfPlanting = new DateTime(2013, 02, 20);
+                crop.WateringFrequency = 2;
+                crop.WateringPeriod = "mjesec";
+                
+                client.InsertCropData(crop);
+
+                Console.WriteLine("The WCF service called returned: ");
+
+                //GetDataUsingDataContract test
+                //CompositeType compositeType = new CompositeType();
+                //compositeType.BoolValue = true;
+                //compositeType.StringValue = "test";
+                //CompositeType responseComposit = client.GetDataUsingDataContract(compositeType);
+
+                //Console.WriteLine("The WCF service called returned: '{0}' '{1}'",
+                //                    responseComposit.BoolValue, responseComposit.StringValue);
+
+                Console.WriteLine("Press any key to close");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception encounter: {0}",
+                                   e.Message);
+            }
+            finally
+            {
+                if (null != client)
+                {
+                    client.Close();
+                }
+            }
+        }
+    }
+}
---
+++ cloud/WindowsAzure2/WCFServiceWebRole1/Service1.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFServiceWebRole1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public void InsertCropData(Crop crop)
+        {
+            using (SqlConnection con = DBConnection.GetConnection())
+            {
+                SqlCommand cmd = new SqlCommand("InsertCrop", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@name", crop.Name));
+                cmd.Parameters.Add(new SqlParameter("@crop_type", crop.CropType));
+                cmd.Parameters.Add(new SqlParameter("@time_of_planting", crop.timeOfPlanting));
+                cmd.Parameters.Add(new SqlParameter("@avatar_image", @"C:\asdfasd.jpg"));
+                cmd.Parameters.Add(new SqlParameter("@watering_frequency", crop.WateringFrequency));
+                cmd.Parameters.Add(new SqlParameter("@watering_period", crop.WateringPeriod));
+                cmd.Parameters.Add(new SqlParameter("@harvest_time", crop.HarvestTime));
+                cmd.Parameters.Add(new SqlParameter("@hilling_time", crop.HillingTime));
+                cmd.Parameters.Add(new SqlParameter("@fertillizing_time", crop.FertilizingTime));
+                cmd.Parameters.Add(new SqlParameter("@illness_foreign_key", crop.IllnessFK));
+                cmd.Parameters.Add(new SqlParameter("@field_foreign_key", crop.FieldFK));
+                cmd.Parameters.Add(new SqlParameter("@journal_foreign_key", crop.JournalFK));
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            //DBConnection.getConnectionStatus();
+            return composite;
+        }
+    }
+}
