ac5f9c7b8c2f8940d1b17adab5af8602e0c81abb
Vladimir Marinkovic <marinkovicvlado@gmail.com>
2013-02-04 15:16:17 +0100

    Documentation and DB backup

---
+++ docs/App mockup/Storyboard_files/story.js
@@ -0,0 +1,369 @@
+var story = {
+ "title": "Storyboard",
+ "pages": [
+  {
+   "title": "MainScreen",
+   "height": 1077,
+   "width": 576,
+   "image": "1-MainScreen.png",
+   "links": [
+    {
+     "page": 7,
+     "rect": [
+      81,
+      232,
+      265,
+      427
+     ]
+    },
+    {
+     "page": 9,
+     "rect": [
+      296,
+      232,
+      480,
+      427
+     ]
+    },
+    {
+     "page": 11,
+     "rect": [
+      81,
+      488,
+      265,
+      683
+     ]
+    },
+    {
+     "page": 14,
+     "rect": [
+      296,
+      488,
+      480,
+      683
+     ]
+    }
+   ]
+  },
+  {
+   "title": "AddCrop",
+   "height": 1077,
+   "width": 576,
+   "image": "2-AddCrop.png",
+   "links": [{
+    "page": 3,
+    "rect": [
+     90,
+     120,
+     277,
+     187
+    ]
+   }]
+  },
+  {
+   "title": "AddCrop-Maintenance",
+   "height": 1077,
+   "width": 576,
+   "image": "3-AddCrop-Maintenance.png",
+   "links": [
+    {
+     "page": 6,
+     "rect": [
+      344,
+      745,
+      440,
+      786
+     ]
+    },
+    {
+     "page": 7,
+     "rect": [
+      147,
+      820,
+      243,
+      861
+     ]
+    },
+    {
+     "page": 7,
+     "rect": [
+      312,
+      820,
+      408,
+      861
+     ]
+    }
+   ]
+  },
+  {
+   "title": "AddCrop-SelectField",
+   "height": 1077,
+   "width": 576,
+   "image": "4-AddCrop-SelectField.png",
+   "links": [{
+    "page": 2,
+    "rect": [
+     90,
+     120,
+     277,
+     187
+    ]
+   }]
+  },
+  {
+   "title": "AddField",
+   "height": 1077,
+   "width": 576,
+   "image": "5-AddField.png",
+   "links": [{
+    "page": 5,
+    "rect": [
+     90,
+     120,
+     273,
+     187
+    ]
+   }]
+  },
+  {
+   "title": "AddField-SelectArea",
+   "height": 1077,
+   "width": 576,
+   "image": "6-AddField-SelectArea.png",
+   "links": [
+    {
+     "page": 4,
+     "rect": [
+      96,
+      128,
+      279,
+      195
+     ]
+    },
+    {
+     "page": 7,
+     "rect": [
+      152,
+      816,
+      216,
+      869
+     ]
+    },
+    {
+     "page": 7,
+     "rect": [
+      368,
+      816,
+      432,
+      869
+     ]
+    }
+   ]
+  },
+  {
+   "title": "AddIllness",
+   "height": 1077,
+   "width": 576,
+   "image": "7-AddIllness.png",
+   "links": [
+    {
+     "page": 2,
+     "rect": [
+      136,
+      836,
+      232,
+      877
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      301,
+      836,
+      397,
+      877
+     ]
+    }
+   ]
+  },
+  {
+   "title": "CropManager",
+   "height": 1077,
+   "width": 576,
+   "image": "8-CropManager.png",
+   "links": [
+    {
+     "page": 1,
+     "rect": [
+      90,
+      288,
+      252,
+      449
+     ]
+    },
+    {
+     "page": 12,
+     "rect": [
+      304,
+      288,
+      466,
+      449
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      104,
+      584,
+      266,
+      745
+     ]
+    },
+    {
+     "page": 13,
+     "rect": [
+      312,
+      584,
+      474,
+      745
+     ]
+    }
+   ]
+  },
+  {
+   "title": "CropPreview",
+   "height": 1077,
+   "width": 576,
+   "image": "9-CropPreview.png",
+   "links": []
+  },
+  {
+   "title": "Journal",
+   "height": 1077,
+   "width": 576,
+   "image": "10-Journal.png",
+   "links": [
+    {
+     "page": 10,
+     "rect": [
+      355,
+      736,
+      421,
+      784
+     ]
+    },
+    {
+     "page": 8,
+     "rect": [
+      160,
+      736,
+      242,
+      784
+     ]
+    }
+   ]
+  },
+  {
+   "title": "JournalEdit",
+   "height": 1077,
+   "width": 576,
+   "image": "11-JournalEdit.png",
+   "links": [
+    {
+     "page": 6,
+     "rect": [
+      318,
+      650,
+      354,
+      692
+     ]
+    },
+    {
+     "page": 0,
+     "rect": [
+      164,
+      792,
+      220,
+      850
+     ]
+    },
+    {
+     "page": 0,
+     "rect": [
+      327,
+      792,
+      383,
+      850
+     ]
+    }
+   ]
+  },
+  {
+   "title": "Notifications",
+   "height": 1077,
+   "width": 576,
+   "image": "12-Notifications.png",
+   "links": []
+  },
+  {
+   "title": "RemoveCrop",
+   "height": 1077,
+   "width": 576,
+   "image": "13-RemoveCrop.png",
+   "links": [
+    {
+     "page": 7,
+     "rect": [
+      160,
+      744,
+      261,
+      792
+     ]
+    },
+    {
+     "page": 7,
+     "rect": [
+      304,
+      744,
+      406,
+      792
+     ]
+    }
+   ]
+  },
+  {
+   "title": "RemoveField",
+   "height": 1077,
+   "width": 576,
+   "image": "14-RemoveField.png",
+   "links": [
+    {
+     "page": 7,
+     "rect": [
+      160,
+      744,
+      261,
+      792
+     ]
+    },
+    {
+     "page": 7,
+     "rect": [
+      304,
+      744,
+      406,
+      792
+     ]
+    }
+   ]
+  },
+  {
+   "title": "WeatherForcast",
+   "height": 1077,
+   "width": 576,
+   "image": "15-WeatherForcast.png",
+   "links": []
+  }
+ ],
+ "highlightLinks": false
+}
